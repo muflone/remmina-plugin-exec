e98af253519594ca51060ec649cccaa483895f68
Muflone <muflone@vbsimple.net>
2013-05-01 14:32:45 +0200

    First code commit

---
+++ remmina-plugin-exec/src/plugin_config.h
@@ -0,0 +1,8 @@
+#ifndef __PLUGIN_CONFIG_H
+#define __PLUGIN_CONFIG_H
+
+#define PLUGIN_NAME        "EXEC"
+#define PLUGIN_DESCRIPTION "EXEC - Execute an external process"
+#define PLUGIN_VERSION     "1.0.0.0"
+#define PLUGIN_APPICON     "remmina-exec"
+#endif
