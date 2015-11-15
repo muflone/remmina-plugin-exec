d64a6ca9d936d08da2d19014d60153c7f2178a2b
Muflone <webreg@vbsimple.net>
2015-11-15 21:03:06 +0100

    Bumped version number

--- remmina-plugin-exec/src/plugin_config.h
+++ remmina-plugin-exec/src/plugin_config.h
@@ -24,6 +24,6 @@
 
   #define PLUGIN_NAME        "EXEC"
   #define PLUGIN_DESCRIPTION "EXEC - Execute an external process"
-  #define PLUGIN_VERSION     "1.2.1.0"
+  #define PLUGIN_VERSION     "1.2.2.0"
   #define PLUGIN_APPICON     "remmina-exec"
 #endif
